00e54a120817b05ec2a47d64b3b290f80be52cd5
el-ahmdo <aliyua875@gmail.com>
2025-07-05 16:35:50 +0100

    final commit hopefully

---
+++ constants/index.ts
@@ -0,0 +1,8 @@
+// Example constants (can be expanded later)
+
+export const API_BASE_URL = "https://api.example.com";
+
+export const UI_TEXT = {
+  bookNow: "Book Now",
+  viewDetails: "View Details",
+};
